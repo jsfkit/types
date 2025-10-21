7a90a9a52a16bdfa4ae14be77e6c2350b6dfb5f6
Matt Riggott <matt.riggott@gmail.com>
2025-10-21 13:03:51 +0000

    Use @borgar/eslint-config in place of Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import borgarLint from "@borgar/eslint-config";
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
@@ -8,7 +9,16 @@ export default defineConfig([
     files: ["**/*.ts"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: { projectService: { allowDefaultProject: [] } },
+    },
   },
   tseslint.configs.recommended,
+  borgarLint.config.recommended,
+  borgarLint.config.stylistic({
+    commaDangle: true,
+    singleBlocks: true,
+    lineLength: 100,
+  }),
 ]);
