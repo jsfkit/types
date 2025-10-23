9170c61a7f3e272936d8f6925c2b50588098f85c
Matt Riggott <matt.riggott@gmail.com>
2025-10-23 17:27:30 +0000

    Publish docs to GitHub Pages
    Builds docs based on TSDoc comments using Typedoc, and publishes the
    HTML it outputs as a GitHub Pages site.
    
    The changes I've made to the TSDoc comments are to improve how
    everything looks in the rendered documentation.

--- src/types/cell_range.ts
+++ src/types/cell_range.ts
@@ -1,7 +1,7 @@
 /**
  * A cell coordinate range in an uppercase A1-style reference format (e.g. `H6:J36`).
  *
- * The range consists of two {@link CellId}s separated by a colon (`:`) character.
+ * The range consists of two {@link CellId | CellIds} separated by a colon (`:`) character.
  *
  * @pattern ^([A-Z]{1,3}[0-9]{1,3}):([A-Z]{1,3}[0-9]{1,3})$
  */
--- src/types/styles/h_align.ts
+++ src/types/styles/h_align.ts
@@ -1,26 +1,33 @@
 /**
  * Specifies the horizontal alignment of content (text) within a container (cell).
- *
- * - `center`: the horizontal alignment is centered, meaning the text is centered across the cell.
- * - `centerContinuous`: the horizontal alignment is centered across multiple cells.
- * - `distributed`: indicates that each 'word' in each line of text inside the cell is evenly
- *   distributed across the width of the cell, with flush right and left margins.
- * - `fill`: indicates that the value of the cell should be filled across the entire width of the
- *   cell. If blank cells to the right also have the fill alignment, they are also filled with the
- *   value, using a convention similar to `centerContinuous`.
- * - `general`: the horizontal alignment is general-aligned. Text data is left-aligned. Numbers,
- *   dates, and times are right- aligned. Boolean types are centered.
- * - `justify`: for each line of text, aligns each line of the wrapped text in a cell to the right
- *   and left (except the last line).
- * - `left`: aligns content at the left edge of the cell (even in RTL mode).
- * - `right`: aligns content at the right edge of the cell (even in RTL mode).
  */
 export type HAlign =
+  /**
+   * The horizontal alignment is general-aligned. Text data is left-aligned. Numbers, dates, and
+   * times are right- aligned. Boolean types are centered.
+   */
   'general' |
+  /** Aligns content at the left edge of the cell (even in RTL mode). */
   'left' |
+  /** The horizontal alignment is centered, meaning the text is centered across the cell. */
   'center' |
+  /** Aligns content at the right edge of the cell (even in RTL mode). */
   'right' |
+  /**
+   * Indicates that the value of the cell should be filled across the entire width of the cell. If
+   * blank cells to the right also have the fill alignment, they are also filled with the value,
+   * using a convention similar to `centerContinuous`.
+   */
   'fill' |
+  /**
+   * For each line of text, aligns each line of the wrapped text in a cell to the right and left
+   * (except the last line).
+   */
   'justify' |
+  /** The horizontal alignment is centered across multiple cells. */
   'centerContinuous' |
+  /**
+   * Indicates that each 'word' in each line of text inside the cell is evenly distributed across
+   * the width of the cell, with flush right and left margins.
+   */
   'distributed';
