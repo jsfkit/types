3457fefe32b42965b989082e96aa6950ee39b6b6
Matt Riggott <matt.riggott@gmail.com>
2026-01-15 11:49:57 +0000

    Add table documenting CellValueType values
    We're missing a reference explaining the possible values of
    CellValueType ('b', 'e', 'n', 'd', 's', 'z').

--- src/types/cell_value_type.ts
+++ src/types/cell_value_type.ts
@@ -1,5 +1,14 @@
 /**
  * Defines the type of a value contained within a cell.
+ *
+ * | Value | Type    | Notes                                                                    |
+ * |-------|---------|--------------------------------------------------------------------------|
+ * | `b`   | Boolean |                                                                          |
+ * | `e`   | Error   | Formula error (cell value is the error code as a string)                 |
+ * | `n`   | Number  | Either integer or float                                                  |
+ * | `d`   | Date    | The {@link Cell.s | cell style} formats the integer cell value as a date |
+ * | `s`   | Text    |                                                                          |
+ * | `z`   | Empty   | Cell is empty                                                            |
  */
 export type CellValueType =
   'b' |
