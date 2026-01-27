61f6cbde76c239b35f2891f6e49fe967dd300828
Matt Riggott <matt.riggott@gmail.com>
2026-01-27 13:03:35 +0000

    Remove id property from Mention type
    Mentions are referenced from anywhere, so the id is superfluous.

--- src/types/threaded_comments/mention.ts
+++ src/types/threaded_comments/mention.ts
@@ -5,8 +5,6 @@ import type { integer } from '../integer';
  * A mention of a person within a threaded comment's text.
  */
 export type Mention = {
-  /** Unique identifier for this mention */
-  id: GUID;
   /** Unique identifier of the person being mentioned. Use this to find the person's details in a
    * {@link Workbook.people | workbook's list of people}.
    */
