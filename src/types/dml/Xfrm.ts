66d6d6d72e25abb0acae9360bf47f7dea81c9ad4
Borgar <borgar@borgar.net>
2026-02-20 10:42:03 +0000

    Use FlipAxis in Xfrm rather than separate props for the axes.

--- src/types/dml/Xfrm.ts
+++ src/types/dml/Xfrm.ts
@@ -1,26 +1,36 @@
 import type { Angle } from './Angle.ts';
 import type { Extent } from './Extent.ts';
 import type { Position } from './Position.ts';
+import type { FlipAxis } from './shape/index.ts';
 
 /**
  * Transform directions to be applied to drawn graphic objects.
  */
 export type Xfrm = {
-  // XXX: normalize to something like { x: true, y: true }?
-  /** Flip horizontally */
-  flipH?: boolean,
-  /** Flip vertically */
-  flipV?: boolean,
-  /** Rotation angle in 60,000ths of a degree  */
+  /**
+   * Which axes the shape should be flipped about.
+   * @defaultValue "none"
+   */
+  flip: FlipAxis,
+
+  /**
+   * Rotation angle in 60,000ths of a degree.
+   * @defaultValue 0
+   */
   rot?: Angle,
-  /** Offset/position of the graphic object */
+
+  /**
+   * Offset/position of the graphic object.
+   */
   off?: Position,
-  /** Extents/size of the graphic object */
+
+  /** Extents/size of the graphic object. */
   ext?: Extent,
 
   // XXX: next two props only exist on groups, split to "GroupTransform2D"?
   /** Child offset - position of child elements within a group (only for groups) */
   chOff?: Position;
+
   /** Child extents - size of the child coordinate system within a group (only for groups) */
   chExt?: Extent;
 };
