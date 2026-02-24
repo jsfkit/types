d5db20fab551b95f011818210318cd936d614e47
Gunnlaugur Thor Briem <gunnlaugur@gmail.com>
2026-02-24 16:31:12 +0000

    Make CalcProps.iterate* properties optional

--- src/types/workbooks/CalcProps.ts
+++ src/types/workbooks/CalcProps.ts
@@ -8,17 +8,17 @@ export type CalcProps = {
    * Specifies whether an attempt should be made to calculate formulas that contain circular
    * references. Defaults to `false` in Excel.
    */
-  iterate: boolean;
+  iterate?: boolean;
   /**
    * The maximum number of calculation iterations, when {@link CalcProps.iterate} is `true`.
    * Defaults to `100` in Excel.
    */
-  iterateCount: integer;
+  iterateCount?: integer;
   /**
    * When a calculation iteration results in an absolute change that is less than iterateDelta,
    * then no further iterations should be attempted. Defaults to `0.001` in Excel.
    */
-  iterateDelta: number;
+  iterateDelta?: number;
   /**
    * Which of the two date systems the workbook uses. 1900 is the default.
    *
