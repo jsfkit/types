e0ad4477c60fee03df0eee4c917956cadafd8e1c
Borgar <borgar@borgar.net>
2026-03-17 11:35:32 +0000

    Reapply "Fix required issue in defined names"
    This reverts commit 5811facb46b9dbd4be37e59a6c6578942eb6b45a.

--- src/types/ExternalDefinedName.ts
+++ src/types/ExternalDefinedName.ts
@@ -11,7 +11,7 @@
  *
  * @group Workbooks
  */
-export type DefinedName = {
+export type ExternalDefinedName = {
   /**
    * A case-sensitive name. Names must start with a letter or `_`, and may only be made up of
    * letters as well as `\`, `_`, `.`, or `?`. Names must be a valid A1 or R1C1 reference.
@@ -21,14 +21,10 @@ export type DefinedName = {
    * A formula expression, a reference, or value. Whatever the value is will be evaluated by a
    * spreadsheet engine as if it were an expression.
    */
-  value: string;
+  value?: string;
   /**
    * An optional worksheet name that defines the scope for this name. When this field is absent,
    * the defined name should be considered to be scoped to the workbook.
    */
   scope?: string;
-  /**
-   * An optional comment explaining the defined name.
-   */
-  comment?: string;
 };
