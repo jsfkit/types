65af4cee2613cbbd70abc294e567dcc1f534f489
Matt Riggott <matt.riggott@gmail.com>
2026-09-14 15:03:10 +0000

    Fix CellOffset JSDoc: row/col props are 0-indexed (#64)
    Just an error in the docs. The xlsx-convert reader implementation has
    always treated these as zero-indexed.
    
    https://github.com/borgar/xlsx-convert/blob/160cf0d5916fbe3086a17f98dd005dd3a3baaad1/src/handler/drawings/readCellPos.ts

--- src/types/dml/CellOffset.ts
+++ src/types/dml/CellOffset.ts
@@ -6,13 +6,13 @@ import type { EmuValue } from './EmuValue.ts';
  * @group Drawings
  */
 export type CellOffset = {
-  /** The row in which the point occurs, 1 indexed. */
+  /** The row in which the point occurs, zero-indexed. */
   row: number,
 
   /** The offset within the row in which the point occurs, in EMUs. */
   rowOff: EmuValue,
 
-  /** The column in which the point occurs, 1 indexed. */
+  /** The column in which the point occurs, zero-indexed. */
   col: number,
 
   /** The position within the column in which the point occurs, in EMUs. */
